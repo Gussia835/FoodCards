174017497a281fcd208f83a2fc9d3aec67f5705f
Gussia835 <146509661+Gussia835@users.noreply.github.com>
2026-08-06 17:09:31 +0300

    add FileService and ShedulerService - add SHedulerService for process sheduledTask - add FileService for creating files

--- src/main/java/ru/mealcard/service/FileService.java
+++ src/main/java/ru/mealcard/service/FileService.java
@@ -1,4 +1,38 @@
 package ru.mealcard.service;
 
-public class FileService {
+import ru.mealcard.Base;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileService extends Base {
+    private static final FileService instance = new FileService();
+    private static final Path output_dir = Path.of(getConfig().getOutputDir());
+    private static final String charset = getConfig().getCharset();
+
+    public static FileService getInstance() {
+        return instance;
+    }
+
+    private FileService() {
+        try {
+            Files.createDirectories(output_dir);
+            info("directory create");
+        } catch (IOException e) {
+            error("cant create directories", e, e.getMessage());
+        }
+    }
+
+    public void save(String filename, String content) {
+        try {
+            Files.write(output_dir.resolve(filename),
+                        content.getBytes(Charset.forName(charset)));
+            info("file created {}", filename);
+        } catch (IOException e) {
+            error("cant create file with content", e.getMessage(), e);
+            throw new IllegalStateException("File saved error", e);
+        }
+    }
 }
--- src/main/java/ru/mealcard/service/ShedulerService.java
+++ src/main/java/ru/mealcard/service/ShedulerService.java
@@ -1,4 +1,34 @@
 package ru.mealcard.service;
 
-public class ShedulerService {
+import lombok.Getter;
+import ru.mealcard.Base;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ShedulerService extends Base {
+
+    private final ScheduledExecutorService sheduler = Executors.newScheduledThreadPool(1);
+    @Getter
+    private static final ShedulerService instance = new ShedulerService();
+
+    private ShedulerService() {
+    }
+
+    public void shedule(LocalDateTime sheduledTime, Runnable task) {
+        long diff = Duration.between(LocalDateTime.now(ZoneId.of(getConfig().getZone())), sheduledTime).toMillis();
+
+        if (diff > 0) {
+            sheduler.schedule(task, diff, TimeUnit.MILLISECONDS);
+        } else {
+            task.run();
+        }
+    }
+
+
+
 }
