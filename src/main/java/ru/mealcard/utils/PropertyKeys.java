ba5e6c320260db6a288684dcfe5ea05c42e150f9
Gussia835 <146509661+Gussia835@users.noreply.github.com>
2026-08-13 15:45:26 +0300

    change names keys of app.properties

--- src/main/java/ru/mealcard/utils/PropertyKeys.java
+++ src/main/java/ru/mealcard/utils/PropertyKeys.java
@@ -5,9 +5,9 @@ public final class PropertyKeys {
     private PropertyKeys() {}
 
     public static final String FILE_PROPERTY =  "/app.properties";
-    public static final String PORT_KEY = "port";
-    public static final String OUTPUT_DIR_KEY = "output-dir";
-    public static final String ZONE_KEY = "zone";
-    public static final String CHARSET_KEY = "charset";
-    public static final String POOL_SIZE_KEY = "pool-size";
+    public static final String PORT_KEY = "server.port";
+    public static final String OUTPUT_DIR_KEY = "GeneratorFile.output-dir";
+    public static final String ZONE_KEY = "timezone.zone";
+    public static final String CHARSET_KEY = "GeneratorFile.charset";
+    public static final String POOL_SIZE_KEY = "server.pool-size";
 }
